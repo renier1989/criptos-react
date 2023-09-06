574b49f107088aa9068720ed70157ee21ec53e3c
Renier Vargas <renier1989@gmail.com>
2023-09-05 20:21:07 -0500

    Class137. Escribiendo el CSS de nuestro proyecto con Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 import styled from "@emotion/styled";
+import ImageCrypto from "./img/imagen-criptos.png";
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    columns-gap: 2rem;
+  }
+`;
 
 const Heading = styled.h1`
-  font-family: 'Lato' , sans-serif;
-  color: #FFFFFF;
+  font-family: "Lato", sans-serif;
+  color: #ffffff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content: '';
+    width:100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `;
 
 function App() {
   return (
-    <div>
-      <Heading> mi pagina de criptomonedas</Heading>
-    </div>
+    <Container>
+      <Image src={ImageCrypto} alt="ImageCryto" />
+      <div>
+        <Heading> Quote your cryptocurrencies instantly</Heading>
+      </div>
+    </Container>
   );
 }
 
