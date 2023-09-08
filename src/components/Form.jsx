ae381ceab7edbc3f52dd0e3a9ad55fb53a85c623
Renier Vargas <renier1989@gmail.com>
2023-09-07 19:20:14 -0500

    Class142. Styled Components al Select

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,6 +13,7 @@ text-transform: uppercase;
 border-radius : 5px;
 font-size: 20px;
 transition:  background-color .3s ease;
+margin-top: 20px;
 
 &:hover{
     background-color: #7A7DFE;
--- src/hooks/useSelectCurrency.jsx
+++ src/hooks/useSelectCurrency.jsx
@@ -2,18 +2,31 @@ import styled from "@emotion/styled";
 
 const Label = styled.label`
 color: #FFF;
+display: block;
+font-family: 'Lato', sans-serif;
+font-size: 24px;
+font-weight: 700;
+margin: 15px 0;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  font-size: 18px;
+  padding: 14px;
+  border-radius: 10px;
+  margin-bottom: 20px;
 `;
 
 const useSelectCurrency = (label, options) => {
   const SelectCurrency = () => (
     <>
       <Label>{label}</Label>
-      <select >
+      <Select >
         <option value="">..Select..</option>
         {options.map(option => (
           <option key={option.id} value={option.id} >{option.name}</option>
         ))}
-      </select>
+      </Select>
     </>
   );
 
