0db819505c3f67f125a92a1df4ef5c4d06b1f100
renier1989 <renier1989@gmail.com>
2023-09-08 08:12:48 -0500

    office

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import styled from "@emotion/styled";
+import ImgCryto from "./img/imagen-criptos.png"
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`;
 
 const Heading = styled.h1`
   font-family: 'Lato' , sans-serif;
   color: #FFFFFF;
 `;
 
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
 function App() {
   return (
-    <div>
+    <Container>
       <Heading> mi pagina de criptomonedas</Heading>
-    </div>
+    </Container>
   );
 }
 
