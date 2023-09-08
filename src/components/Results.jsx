1ee9b6ee174deae20a2fdc4d35c560660f534d32
renier1989 <renier1989@gmail.com>
2023-09-08 09:18:38 -0500

    Class151. Formateando el Resultado

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,14 +1,60 @@
-const Results = ({quote}) => {
+import styled from "@emotion/styled";
 
-    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE, IMAGEURL} =  quote;
-    console.log(quote);
-  return <div>
-    <p>Price : {PRICE}</p>
-    <p>High Day : {HIGHDAY}</p>
-    <p>Low Day : {LOWDAY}</p>
-    <p>Change 24 hours : {CHANGEPCT24HOUR}</p>
-    <p>Last Update : {LASTUPDATE}</p>
-  </div>;
+const Result = styled.div`
+  color: #ffff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Text = styled.p`
+  font-size: 18px;
+  span: {
+    font-weight: 700;
+  }
+`;
+
+const Price = styled.p`
+  font-size: 24px;
+  span: {
+    font-weight: 700;
+  }
+`;
+
+const Image = styled.img`
+display: block;
+width: 120px;
+
+`;
+
+const Results = ({ quote }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE, IMAGEURL } =
+    quote;
+  console.log(quote);
+  return (
+    <Result>
+      <Image src={`https://cryptocompare.com/${IMAGEURL}`} alt="image-crypto" />
+      <div>
+        <Price>
+          Price : <span>{PRICE}</span>
+        </Price>
+        <Text>
+          High Day : <span>{HIGHDAY}</span>
+        </Text>
+        <Text>
+          Low Day : <span>{LOWDAY}</span>
+        </Text>
+        <Text>
+          Change 24 hours : <span>{CHANGEPCT24HOUR}</span>
+        </Text>
+        <Text>
+          Last Update : <span>{LASTUPDATE}</span>
+        </Text>
+      </div>
+    </Result>
+  );
 };
 
 export default Results;
